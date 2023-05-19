91240491fd7b04c85871d61a8a6e573007ab2373
rinceoir <voronin.adm@mail.ru>
2023-05-19 21:58:20 +0300

    make ingredients-selector

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -15,43 +15,11 @@
             </h2>
             <div class="sheet__content ingredients">
               <sauce-selector v-model="pizza.sauce" :sauces="sauces" />
-              <div class="ingredients__filling">
-                <p>Начинка:</p>
 
-                <ul class="ingredients__list">
-                  <li
-                    v-for="ingredient in ingredients"
-                    :key="ingredient.id"
-                    class="ingredients__item"
-                  >
-                    <span :class="`filling filling--${ingredients.value}`">{{
-                      ingredient.name
-                    }}</span>
-
-                    <div class="counter counter--orange ingredients__counter">
-                      <button
-                        type="button"
-                        class="counter__button counter__button--minus"
-                        disabled
-                      >
-                        <span class="visually-hidden">Меньше</span>
-                      </button>
-                      <input
-                        type="text"
-                        name="counter"
-                        class="counter__input"
-                        value="0"
-                      />
-                      <button
-                        type="button"
-                        class="counter__button counter__button--plus"
-                      >
-                        <span class="visually-hidden">Больше</span>
-                      </button>
-                    </div>
-                  </li>
-                </ul>
-              </div>
+              <ingredients-selector
+                v-model="pizza.ingredients"
+                :ingredients="ingredients"
+              />
             </div>
           </div>
         </div>
@@ -109,16 +77,17 @@ const sizes = sizesJSON.map(normalizeSize);
 import DoughSelector from "../modules/constructor/DoughSelector.vue";
 import SizeSelector from "../modules/constructor/SizeSelector.vue";
 import SauceSelector from "../modules/constructor/SauceSelector.vue";
+import IngredientsSelector from "../modules/constructor/IngredientsSelector.vue";
 
 const pizza = reactive({
   name: "",
   dough: doughs[0].value,
   size: sizes[1].value,
   sauce: sauces[0].value,
-  // ingredients: ingredientItems.reduce((acc, item) => {
-  //   acc[item.value] = 0;
-  //   return acc;
-  // }, {}),
+  ingredients: ingredients.reduce((acc, item) => {
+    acc[item.value] = 0;
+    return acc;
+  }, {}),
 });
 </script>
 
@@ -207,19 +176,6 @@ const pizza = reactive({
   border-top: 1px solid rgba($green-500, 0.1);
 }
 
-
-
-.ingredients__filling {
-  width: 100%;
-
-  p {
-    @include r-s16-h19;
-
-    margin-top: 0;
-    margin-bottom: 16px;
-  }
-}
-
 .title {
   box-sizing: border-box;
   width: 100%;
@@ -234,27 +190,6 @@ const pizza = reactive({
   &--small {
     @include b-s18-h21;
   }
-}
-
-.ingredients__list {
-  @include clear-list;
-
-  display: flex;
-  align-items: flex-start;
-  flex-wrap: wrap;
-}
-
-.ingredients__item {
-  width: 100px;
-  min-height: 40px;
-  margin-right: 17px;
-  margin-bottom: 35px;
-}
-
-.ingredients__counter {
-  width: 54px;
-  margin-top: 10px;
-  margin-left: 36px;
 }
 
 .radio {
@@ -316,343 +251,6 @@ const pizza = reactive({
         }
       }
     }
-  }
-}
-
-.counter {
-  display: flex;
-
-  justify-content: space-between;
-  align-items: center;
-}
-
-.counter__button {
-  $el: &;
-  $size_icon: 50%;
-
-  position: relative;
-
-  display: block;
-
-  width: 16px;
-  height: 16px;
-  margin: 0;
-  padding: 0;
-
-  cursor: pointer;
-  transition: 0.3s;
-
-  border: none;
-  border-radius: 50%;
-  outline: none;
-
-  &--minus {
-    background-color: $purple-100;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-
-      border-radius: 2px;
-      background-color: $black;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $purple-200;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $purple-300;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      &::before {
-        opacity: 0.1;
-      }
-    }
-  }
-
-  &--plus {
-    background-color: $green-500;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &::after {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-      transform: translate(-50%, -50%) rotate(90deg);
-
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $green-400;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $green-600;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      opacity: 0.3;
-    }
-  }
-
-  &--orange {
-    background-color: $orange-200;
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $orange-100;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $orange-300;
-    }
-  }
-}
-
-.counter__input {
-  @include r-s14-h16;
-
-  box-sizing: border-box;
-  width: 22px;
-  margin: 0;
-  padding: 0 3px;
-
-  text-align: center;
-
-  color: $black;
-  border: none;
-  border-radius: 10px;
-  outline: none;
-  background-color: transparent;
-
-  &:focus {
-    box-shadow: inset $shadow-regular;
-  }
-}
-
-.filling {
-  @include r-s14-h16;
-
-  position: relative;
-
-  display: block;
-
-  padding-left: 36px;
-
-  &::before {
-    @include p_center-v;
-
-    display: block;
-
-    width: 32px;
-    height: 32px;
-
-    content: "";
-
-    border-radius: 50%;
-    background-color: $white;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: 80% 80%;
-  }
-
-  &--tomatoes::before {
-    background-image: url("@/assets/img/filling/tomatoes.svg");
-  }
-
-  &--ananas::before {
-    background-image: url("@/assets/img/filling/ananas.svg");
-  }
-
-  &--bacon::before {
-    background-image: url("@/assets/img/filling/bacon.svg");
-  }
-
-  &--blue_cheese::before {
-    background-image: url("@/assets/img/filling/blue_cheese.svg");
-  }
-
-  &--cheddar::before {
-    background-image: url("@/assets/img/filling/cheddar.svg");
-  }
-
-  &--chile::before {
-    background-image: url("@/assets/img/filling/chile.svg");
-  }
-
-  &--ham::before {
-    background-image: url("@/assets/img/filling/ham.svg");
-  }
-
-  &--jalapeno::before {
-    background-image: url("@/assets/img/filling/jalapeno.svg");
-  }
-
-  &--mozzarella::before {
-    background-image: url("@/assets/img/filling/mozzarella.svg");
-  }
-
-  &--mushrooms::before {
-    background-image: url("@/assets/img/filling/mushrooms.svg");
-  }
-
-  &--olives::before {
-    background-image: url("@/assets/img/filling/olives.svg");
-  }
-
-  &--onion::before {
-    background-image: url("@/assets/img/filling/onion.svg");
-  }
-
-  &--parmesan::before {
-    background-image: url("@/assets/img/filling/parmesan.svg");
-  }
-
-  &--salami::before {
-    background-image: url("@/assets/img/filling/salami.svg");
-  }
-
-  &--salmon::before {
-    background-image: url("@/assets/img/filling/salmon.svg");
-  }
-}
-
-.button {
-  $bl: &;
-
-  @include b-s18-h21;
-  font-family: inherit;
-  display: block;
-
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-
-  cursor: pointer;
-  transition: 0.3s;
-  text-align: center;
-
-  color: $white;
-  border: none;
-  border-radius: 8px;
-  outline: none;
-  box-shadow: $shadow-medium;
-
-  background-color: $green-500;
-
-  &:hover:not(:active):not(:disabled) {
-    background-color: $green-400;
-  }
-
-  &:active:not(:disabled) {
-    background-color: $green-600;
-  }
-
-  &:focus:not(:disabled) {
-    opacity: 0.5;
-  }
-
-  &:disabled {
-    background-color: $green-300;
-    color: rgba($white, 0.2);
-    cursor: default;
-  }
-
-  &--border {
-    background-color: transparent;
-    border: 1px solid $green-500;
-    color: $black;
-    box-shadow: none;
-
-    &:hover:not(:active):not(:disabled) {
-      color: $green-500;
-      border-color: $green-500;
-      background-color: transparent;
-    }
-
-    &:active:not(:disabled) {
-      color: $green-600;
-      border-color: $green-600;
-      background-color: transparent;
-    }
-
-    &:disabled {
-      opacity: 0.5;
-    }
-  }
-
-  &--transparent {
-    @include b-s14-h16;
-    background-color: transparent;
-    box-shadow: none;
-    color: $black;
-
-    &:hover:not(:active):not(:disabled) {
-      color: $red-800;
-      background-color: transparent;
-    }
-
-    &:active:not(:disabled) {
-      color: $red-900;
-      background-color: transparent;
-    }
-
-    &:disabled {
-      opacity: 0.25;
-    }
-  }
-
-  &--arrow {
-    &::before {
-      content: "";
-      background-image: url("@/assets/img/button-arrow.svg");
-      background-position: center;
-      background-repeat: no-repeat;
-      margin-right: 16px;
-      width: 18px;
-      height: 18px;
-      display: inline-block;
-      vertical-align: middle;
-      transform: translateY(-1px);
-    }
-  }
-
-  &--white {
-    background-color: $white;
-    color: $green-500;
   }
 }
 
